dba967a5ebb7e925101ab516ba4c5cec38770ef1
Anaga <bobyrev.bob@gmail.com>
2023-06-27 17:46:06 +0300

    I love secure rust!

--- exercise/a_variables/varaibles/src/main.rs
+++ exercise/a_variables/varaibles/src/main.rs
@@ -1,5 +1,5 @@
 const STARTING_MISSILES: u8 = 8;
-const READY_AMOUNT: u8 = 2;
+const READY_AMOUNT: u8 = 3;
 fn main() {
     let (mut missiles, ready): (u8, u8) = (STARTING_MISSILES, READY_AMOUNT);
     while missiles!=0 {
@@ -7,5 +7,5 @@ fn main() {
         missiles = missiles - ready;
         println!("{} missiles left", missiles); 
     }
-    println!("End of missiles");
+    println!("End of missiles!");
 }
