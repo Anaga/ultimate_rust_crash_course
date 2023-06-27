e556a395a7fd77272be6b421dac0d07ce1ffb8fb
Anaga <bobyrev.bob@gmail.com>
2023-06-27 17:43:19 +0300

    Fire missales in a loop. until end of all ammo.

--- exercise/a_variables/varaibles/src/main.rs
+++ exercise/a_variables/varaibles/src/main.rs
@@ -2,7 +2,10 @@ const STARTING_MISSILES: u8 = 8;
 const READY_AMOUNT: u8 = 2;
 fn main() {
     let (mut missiles, ready): (u8, u8) = (STARTING_MISSILES, READY_AMOUNT);
-    println!("Firing {} of my {} missiles...", ready, missiles);
-    missiles = missiles - ready;
-    println!("{} missiles left", missiles);
+    while missiles!=0 {
+        println!("Firing {} of my {} missiles...", ready, missiles);
+        missiles = missiles - ready;
+        println!("{} missiles left", missiles); 
+    }
+    println!("End of missiles");
 }
