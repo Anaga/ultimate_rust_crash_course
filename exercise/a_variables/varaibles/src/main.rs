c724bda2d9e8caaf5f4cf7f530fce1fc733e1b76
Anaga <bobyrev.bob@gmail.com>
2023-06-27 17:56:25 +0300

    Love sec Rust so much!

--- exercise/a_variables/varaibles/src/main.rs
+++ exercise/a_variables/varaibles/src/main.rs
@@ -1,7 +1,7 @@
-const STARTING_MISSILES: u8 = 8;
-const READY_AMOUNT: u8 = 3;
+const STARTING_MISSILES: i8 = 8;
+const READY_AMOUNT: i8 = 3;
 fn main() {
-    let (mut missiles, ready): (u8, u8) = (STARTING_MISSILES, READY_AMOUNT);
+    let (mut missiles, ready): (i8, i8) = (STARTING_MISSILES, READY_AMOUNT);
     while missiles!=0 {
         println!("Firing {} of my {} missiles...", ready, missiles);
         missiles = missiles - ready;
